862bdd43b81b7cefc75292536d3769d70891f873
Carlos isen <amra_akyra@hotmail.com>
2023-01-26 17:43:13 +0000

    Home created

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import Map2 from './Map2';
+import AsideMenu from "./AsideMenu";
+
+const Home = () => {
+
+    return (
+        <>
+            <AsideMenu />
+            <Map2 />
+        </>
+    )
+}
+export default Home
