01e0d2f25c9d76234cf204dd89961dad34cbfd26
Carlos isen <amra_akyra@hotmail.com>
2023-01-28 12:32:24 +0000

    Geolocation is Active when mapa is load

--- src/Components/Map2.jsx
+++ src/Components/Map2.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef} from "react";
 import Map, {Marker, GeolocateControl} from "react-map-gl"
 import useLoc from "../utils/useLoc";
 import Marks from "../utils/Marks.json"
@@ -6,41 +6,45 @@ import { MdNavigation } from "react-icons/md";
 import "../assets/Map.css"
 
 const Map2= () => {
-    const [viewState, setViewState]= useState({
+// pendiente intentar crear un useLoc solo con current position para encuadrar el mapa al empezar.
+    const initialViewState ={
             latitude: 40.4,
             longitude: -3.68,
             zoom:12,
-            cursor: "auto",
-        })
+        }
     const map2ref= useRef()
+    const geoControl=useRef()
 
-    const centerView= (e= 40.4 , i= -3.68)=>{
-        setViewState({
-            ...viewState,
-            latitude: e,
-            longitude: i,
-            zoom: viewState.zoom + 1
-        })
-    }
+    const centerView = ((e = 40.4, i = -3.68) => {
+        map2ref.current?.flyTo({ center: [i, e], zoom:15, duration: 2000 });
+    })
+
     const isNear= (e)=>{
         console.log(e.coords.latitude, e.coords.longitude)
+    }
+    const activeControl= ()=>{
+        if(geoControl)
+        geoControl.current.trigger()
     }
 
     return (
         <div className="Map">
             <Map
             ref={map2ref}
-            {...viewState}
-            onMove={evt => setViewState(evt.viewState)}
+            initialViewState= {initialViewState}
             mapStyle={"mapbox://styles/mapbox/streets-v9"}
             mapboxAccessToken= {process.env.REACT_APP_MAP_PUBLIC_TOKEN}
+            cursor="auto"
+            // activa la geolocalizacion al cargar el mapa directamente
+            onLoad={activeControl}
             >
                 {/* Geolocalizacion con funcion para usar de trigger por cercania */}
                 <GeolocateControl position="top-right"
                     trackUserLocation="true"
+                    ref= {geoControl}
                     onGeolocate={e=>isNear(e)}/>
 
-                {Marks?.features.map((point) => 
+                {Marks?.features?.map((point) => 
                     <Marker
                     key={point.properties.id}
                     longitude={point.geometry.lng}
@@ -50,11 +54,9 @@ const Map2= () => {
                         <img className="Marker-icon"src="/30.png"></img>
                 </Marker>)}
             </Map>
-            <button onClick={()=> setViewState({
-                ...viewState,
-                latitude: 40.4,
-                longitude: -3.68
-            })} className="Map-icon--navcenter" >center</button>
+            <button onClick={()=> 
+                centerView(40.4, -3.68)} 
+                className="Map-icon--navcenter" >center</button>
             <MdNavigation className="Map-icon--nav"/>
         </div>
     )
