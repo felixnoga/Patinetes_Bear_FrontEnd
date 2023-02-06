d2674e691e4405e2352bb8749e8175d765d68338
PaolaGLupp <gldillana@gmail.com>
2023-02-06 22:25:46 +0100

    Login and Register Components

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,16 +1,69 @@
-import { useNavigate } from "react-router-dom"
-import { useAppContext } from "../context/context"
+import { useNavigate, Link } from "react-router-dom"
+import { useAppContext } from "../context/context";
+import {useState} from 'react';
+import  axios  from 'axios';
+import '../assets/Login.css'
 
 const Login=() => {
     const {log}= useAppContext()
-    const toHome= useNavigate()
+    const toHome= useNavigate();
 
-    const handleClick= ()=>{
-        log()
-        toHome("/")
+    const url= "http://localhost:3005/login"; 
+
+    const [data, setData] = useState({
+        email:"",
+        password:""
+    })
+
+    function login(e) {
+        e.preventDefault(); 
+        if((data.email=="") || (data.password=="")) {
+            if(data.email=="") {
+                alert("falta email")
+            } else if (data.password==""){
+                alert("falta contraseña");
+            }
+            return;
+        } 
+
+
+        axios.post(url, {
+            email: data.email, 
+            password:data.password
+        })
+        .then(res=>{
+                log()
+                toHome("/")
+        });
     }
+
+    function handle(e){
+        const newData = {...data};
+        newData[e.target.id] = e.target.value;
+        setData(newData);
+      }
+
         return(
-            <button onClick={handleClick}> PULSA PARA LOGIN!!!</button>
+            <div className="login-container">
+
+        <div className='login-input'>
+            <input type="text" name="email" placeholder="Email" required onChange={(e)=>handle(e)} id="email" value={data.email}/>
+        </div>
+
+        <div className='login-input'>
+            <input type="password" name="password" placeholder="Contraseña" required onChange={(e)=>handle(e)} id="password" value={data.password}/>
+        </div>
+        <div>
+            <button type='button' className="login-btn" onClick={login}>Login</button>  
+        </div>
+        <p>¿No tienes cuenta todavia? </p>
+        <Link className='link' to="/register">
+        <div>
+            <button type='button' className="login-btn" >Regístrate</button>  
+        </div>
+        </Link>
+
+        </div>
         )
 
 }
