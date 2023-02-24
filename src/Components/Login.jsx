79630f8da271e7183a07b804268e8adb1939e681
Rocio Lanfranconi <rociolanfranconi@gmail.com>
2023-02-24 16:45:52 +0100

    new

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,15 +3,15 @@ import { useAppContext } from "../context/context";
 import {useState} from 'react';
 import '../assets/Login.css'
 
-const Login = () => {
-    const { log } = useAppContext()
-    const toHome = useNavigate();
+const Login=() => {
+    const {log}= useAppContext()
+    const toHome= useNavigate();
 
-    const url = "http://localhost:3005/login";
+    const url= "http://localhost:3005/login"; 
 
     const [data, setData] = useState({
-        email: "",
-        password: ""
+        email:"",
+        password:""
     })
 
     const login = async (e) => {
@@ -20,11 +20,11 @@ const Login = () => {
         if((data.email=="") || (data.password=="")) {
             if(data.email=="") {
                 alert("falta email")
-            } else if (data.password == "") {
+            } else if (data.password==""){
                 alert("falta contraseña");
             }
             return;
-        }
+        } 
 
         const body = {
             email: data.email, 
@@ -55,49 +55,42 @@ const Login = () => {
         }
     }
 
-    function handle(e) {
-        const newData = { ...data };
+    function handle(e){
+        const newData = {...data};
         newData[e.target.id] = e.target.value;
         setData(newData);
-      }
-
-    // pruebas para saltar el login.
-    const autoEnter= ()=>{
-        log()
-        toHome("/")
     }
 
-    return (
-        <>  
-         <div className="title-login2">
-            <h1>Inicia de Sesión</h1>
-        </div>
-            <div className="login-conteiner">
-
-                <div className="big-login-input">
-                    <div>
-                        <input className='login2-input' type="text" name="email" placeholder="Email" required onChange={(e) => handle(e)} id="email" value={data.email} />
-                    </div>
-
-        <div className='login-input'>
-            <input type="password" name="password" placeholder="Contraseña" required onChange={(e)=>handle(e)} id="password" value={data.password}/>
-        </div>
-        <div>
-            <button type='button' className="login-btn" onClick={login}>Login</button>  
-        </div>
-        <p>¿No tienes cuenta todavia? </p>
-        <Link className='link' to="/register">
-        <div>
-            <button type='button' className="login-btn" >Regístrate</button>  
-        </div>
-        </Link>
-        {/* para pruebas solo */}
-                <button type='button' className="login-btn" onClick={autoEnter}>TEST</button> 
-
-            </div>
-        </>
-
-    )
+        return(
+            <>  
+            <div className="title-login2">
+               <h1>Inicia de Sesión</h1>
+           </div>
+               <div className="login-conteiner">
+   
+                   <div className="big-login-input">
+                       <div>
+                           <input className='login2-input' type="text" name="email" placeholder="Email" required onChange={(e) => handle(e)} id="email" value={data.email} />
+                       </div>
+   
+                       <div >
+                           <input className='login2-input' type="password" placeholder="Contraseña" required onChange={(e) => handle(e)} id="password" value={data.password} />
+                       </div>
+                   </div>
+                   <div className="">
+                       <button type='button' className="login2-btn" onClick={login}>Login</button>
+                   </div>
+                   <div className="box-login">
+                   <Link className='link' to="/register">
+                   <h4 className="subtitle-login">¿No tienes cuenta todavia? <b className="reg">Registrate</b> </h4>
+                   </Link>
+                   </div>
+                   {/* para pruebas solo */}
+                   {/* <button type='button' className="login-btn" onClick={autoEnter}>TEST</button> */}
+   
+               </div>
+           </>
+        )
 
 }
 
